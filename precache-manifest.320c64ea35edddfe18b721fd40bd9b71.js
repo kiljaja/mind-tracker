4a71e07e326130366088d87fcc3ef6eab3c64dac
Emilio Franco <ejfranco06@gmail.com>
2020-05-09 16:18:05 -0400

    Updates

---
+++ precache-manifest.320c64ea35edddfe18b721fd40bd9b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4a668deacbf490da7143e4f33a0318",
+    "url": "/ejfranco06/mind-tracker/index.html"
+  },
+  {
+    "revision": "06c2f2836760b74a106d",
+    "url": "/ejfranco06/mind-tracker/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "41f4c887dd89d6dcc3a0",
+    "url": "/ejfranco06/mind-tracker/static/js/2.8688b1f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ejfranco06/mind-tracker/static/js/2.8688b1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c2f2836760b74a106d",
+    "url": "/ejfranco06/mind-tracker/static/js/main.5e8673d5.chunk.js"
+  },
+  {
+    "revision": "770afddd75ef695ef7f4",
+    "url": "/ejfranco06/mind-tracker/static/js/runtime-main.ff509399.js"
+  }
+]);
