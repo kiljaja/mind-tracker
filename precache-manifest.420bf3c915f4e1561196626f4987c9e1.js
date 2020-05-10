e4053eddf08747fdae3652684506016a159608b5
Emilio Franco <ejfranco06@gmail.com>
2020-05-09 23:47:42 -0400

    Updates

---
+++ precache-manifest.420bf3c915f4e1561196626f4987c9e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20175edc8e5a4b882f41fa7df0ef614",
+    "url": "/mind-tracker/index.html"
+  },
+  {
+    "revision": "0931fe27ef2606fbe5ff",
+    "url": "/mind-tracker/static/css/main.54d98d3d.chunk.css"
+  },
+  {
+    "revision": "dd2ba8f5d2053de9d2f1",
+    "url": "/mind-tracker/static/js/2.fde54107.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mind-tracker/static/js/2.fde54107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0931fe27ef2606fbe5ff",
+    "url": "/mind-tracker/static/js/main.89d1338f.chunk.js"
+  },
+  {
+    "revision": "b134b95c09755ae0c413",
+    "url": "/mind-tracker/static/js/runtime-main.c0419002.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mind-tracker/precache-manifest.c569bdfc05ebad5f5dc314c0860d816b.js"
+  "/mind-tracker/precache-manifest.420bf3c915f4e1561196626f4987c9e1.js"
 );
 
 self.addEventListener('message', (event) => {
