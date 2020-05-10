018e7aab4ce593a66f84a3079ea6d36fb667ce98
Emilio Franco <ejfranco06@gmail.com>
2020-05-10 00:55:56 -0400

    Add habit report card

--- src/components/HabitOverview/HabitOverview.js
+++ src/components/HabitOverview/HabitOverview.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import "./HabitOverview.css";
+// TODO: abstract the logo image
+import logoImg from "../../images/baby-groot-meditation.jpeg";
+
 import {
   getStartOfWeek,
   formatDates,
@@ -6,8 +10,12 @@ import {
   createZeroHourDate,
 } from "../../utils/helper-functions";
 
+import WeekReport from "../WeekReport/WeekReport";
+
 const HabitOverview = ({ habitEntries }) => {
-  const date = formatDates(createZeroHourDate("2020-05-05"));
+  //temp value
+  const name = "Meditation";
+
 
   // Get the habits that fall within this current week Monday-Sunday
   const getThisWeeksHabits = (habitEntries = []) => {
@@ -25,80 +33,15 @@ const HabitOverview = ({ habitEntries }) => {
     return weekEntries;
   };
 
-  // get this weeks habit map with a counter for days at least one entry was made 
-  const getWeekHabitMap = (weekEntries = []) => {
-    const weekHabitMap = {
-      count: 0,
-      monday: false,
-      tuesday: false,
-      wednesday: false,
-      thursday: false,
-      friday: false,
-      saturday: false,
-      sunday: false,
-    };
-
-    weekEntries.forEach((entry) => {
-      const date = createZeroHourDate(entry.date);
-      const day = date.getUTCDay();
-
-      switch (day) {
-        case 0:
-          if (!weekHabitMap.sunday) {
-            weekHabitMap.sunday = true;
-            weekHabitMap.count++;
-          }
-          break;
-        case 1:
-          if (!weekHabitMap.monday) {
-            weekHabitMap.monday = true;
-            weekHabitMap.count++;
-          }
-          break;
-        case 2:
-          if (!weekHabitMap.tuesday) {
-            weekHabitMap.tuesday = true;
-            weekHabitMap.count++;
-          }
-          break;
-        case 3:
-          if (!weekHabitMap.wednesday) {
-            weekHabitMap.wednesday = true;
-            weekHabitMap.count++;
-          }
-          break;
-        case 4:
-          if (!weekHabitMap.thursday) {
-            weekHabitMap.thursday = true;
-            weekHabitMap.count++;
-          }
-          break;
-        case 5:
-          if (!weekHabitMap.friday) {
-            weekHabitMap.friday = true;
-            weekHabitMap.count++;
-          }
-          break;
-        case 6:
-          if (!weekHabitMap.saturday) {
-            weekHabitMap.saturday = true;
-            weekHabitMap.count++;
-          }
-          break;
-        default:
-      }
-    });
-
-    return weekHabitMap;
-  };
-
   const weekEntries = getThisWeeksHabits(habitEntries);
-  const weekHabitMap = getWeekHabitMap(weekEntries);
+  
   return (
-    <div>
-      <h1>HabitOverview</h1>
-      <h2> {date} </h2>
-    </div>
+    <button className="habit-overview">
+      <div className="logo-img-container">
+        <img className="logo-img" src={logoImg} alt={`${name} logo`}/>
+      </div>
+      < WeekReport weekEntries={weekEntries} name={name}/>
+    </button>
   );
 };
 
---
+++ src/components/WeekReport/WeekReport.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import "./WeekReport.css";
+
+import {
+  getStartOfWeek,
+  formatDates,
+  getEndOfWeek,
+  createZeroHourDate,
+} from "../../utils/helper-functions";
+
+function WeekReport({ weekEntries = [], name = "habit" }) {
+  // get this weeks habit map with a counter for days at least one entry was made
+  const getWeekHabitMap = (weekEntries = []) => {
+    const weekHabitMap = {
+      count: 0,
+      monday: false,
+      tuesday: false,
+      wednesday: false,
+      thursday: false,
+      friday: false,
+      saturday: false,
+      sunday: false,
+    };
+
+    weekEntries.forEach((entry) => {
+      const date = createZeroHourDate(entry.date);
+      const day = date.getUTCDay();
+
+      switch (day) {
+        case 0:
+          if (!weekHabitMap.sunday) {
+            weekHabitMap.sunday = true;
+            weekHabitMap.count++;
+          }
+          break;
+        case 1:
+          if (!weekHabitMap.monday) {
+            weekHabitMap.monday = true;
+            weekHabitMap.count++;
+          }
+          break;
+        case 2:
+          if (!weekHabitMap.tuesday) {
+            weekHabitMap.tuesday = true;
+            weekHabitMap.count++;
+          }
+          break;
+        case 3:
+          if (!weekHabitMap.wednesday) {
+            weekHabitMap.wednesday = true;
+            weekHabitMap.count++;
+          }
+          break;
+        case 4:
+          if (!weekHabitMap.thursday) {
+            weekHabitMap.thursday = true;
+            weekHabitMap.count++;
+          }
+          break;
+        case 5:
+          if (!weekHabitMap.friday) {
+            weekHabitMap.friday = true;
+            weekHabitMap.count++;
+          }
+          break;
+        case 6:
+          if (!weekHabitMap.saturday) {
+            weekHabitMap.saturday = true;
+            weekHabitMap.count++;
+          }
+          break;
+        default:
+      }
+    });
+
+    return weekHabitMap;
+  };
+
+  const {
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+    sunday,
+    count,
+  } = getWeekHabitMap(weekEntries);
+
+  return (
+    <div className="week-report">
+      <div className="left-report">
+        <h1>{name}</h1>
+        <div className="day-dot-list">
+          <DayDot isGreen={monday} />
+          <DayDot isGreen={tuesday} />
+          <DayDot isGreen={wednesday} />
+          <DayDot isGreen={thursday} />
+          <DayDot isGreen={friday} />
+          <DayDot isGreen={saturday} />
+          <DayDot isGreen={sunday} />
+        </div>
+        <p className="report-text"> {`${count}/7`}</p>
+      </div>
+
+      <div className="right-report">
+        <p>Total</p>
+        <p>{weekEntries.length}</p>
+      </div>
+    </div>
+  );
+}
+
+const DayDot = ({ isGreen = false }) => {
+  return (
+    <span className={`day-dot  ${isGreen ? "day-dot-filled" : ""} `}></span>
+  );
+};
+
+export default WeekReport;
