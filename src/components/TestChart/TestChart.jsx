aceb384f17d189c65009bb3f294195be9e702188
Emilio Franco <ejfranco06@gmail.com>
2020-06-26 17:28:37 -0400

    Style test Chart

--- src/components/TestChart/TestChart.jsx
+++ src/components/TestChart/TestChart.jsx
@@ -8,7 +8,7 @@ import { Line } from 'react-chartjs-2';
 
 const mockData = [
   { date: '2020-06-17', userName: 'demoUser', entries: 13 },
-  { date: '2020-06-17', userName: 'demoUser', entries: 15 },
+  { date: '2020-06-18', userName: 'demoUser', entries: 15 },
   { date: '2020-06-19', userName: 'demoUser', entries: 27 },
   { date: '2020-06-20', userName: 'demoUser', entries: 20 },
   { date: '2020-06-21', userName: 'demoUser', entries: 1 },
@@ -27,21 +27,18 @@ export const TestChart = () => {
     datasets: [
       {
         label: 'Meditation',
-        fill: 'origin',
+        fill: false,
         backgroundColor: 'rgba(75,192,192,0.4)',
         borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
         borderJoinStyle: 'miter',
         pointBorderColor: 'rgba(75,192,192,1)',
         pointBackgroundColor: '#fff',
-        pointBorderWidth: 5,
+        pointBorderWidth: 2,
         pointHoverRadius: 10,
         pointHoverBackgroundColor: 'rgba(75,192,192,1)',
         pointHoverBorderColor: 'rgba(220,220,220,1)',
         pointHoverBorderWidth: 2,
-        pointRadius: 1,
+        pointRadius: 5,
         pointHitRadius: 10,
         data: newData,
       },
